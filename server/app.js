a92de3c30c69ea823e9662c5d5c05a3f5246a6a8
Michael Carvajal <michaelcarvajal.work@gmail.com>
2024-06-26 11:26:00 -0400

    added route to serve build

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 // src/app.js
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const automationRoutes = require('./routes/automation');
 
 const app = express();
@@ -13,6 +14,11 @@ app.use('/api/automation', automationRoutes);
 app.get('/hello', (req, res) => {
   return res.json({message: "hellow world"})
 })
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
