ab73d1bb0391629e239c438c0ed292876c47f7bf
Michael Carvajal <michaelcarvajal.work@gmail.com>
2024-07-15 11:56:10 -0400

    added try catch on fetch request so react app doesnt error out

--- src/App.js
+++ src/App.js
@@ -17,26 +17,30 @@ function App() {
     setError([]);
     console.log(vendor, itemNumber, poNumber, quantity, pickup);
     const storeNumber = poNumber.split("-")[0];
-    const response = await fetch("http://localhost:5000/api/automation/run", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        vendor,
-        storeNumber,
-        itemNumber,
-        poNumber,
-        quantity,
-        pickup,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
-    if (data.error) {
-      setError(data.error);
-    }else if (data.confirmation) {
-      setConfirmation(data.confirmation)
+    try {
+      const response = await fetch("http://localhost:5000/api/automation/run", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          vendor,
+          storeNumber,
+          itemNumber,
+          poNumber,
+          quantity,
+          pickup,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (data.error) {
+        setError(data.error);
+      }else if (data.confirmation) {
+        setConfirmation(data.confirmation)
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
   const Error = (
