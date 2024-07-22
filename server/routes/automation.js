fb40eedf6c17933563395f14e7a0ada8acd972c0
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-07-21 22:09:59 -0400

    moved folders in src folder for electron builder bug, and able to build functioning app

--- server/routes/automation.js
+++
@@ -1,8 +0,0 @@
-// src/routes/automation.js
-const express = require('express');
-const { runAutomation } = require('../controllers/automationController');
-const router = express.Router();
-
-router.post('/run', runAutomation);
-
-module.exports = router;
--- server/src/server.js
+++ server/src/server.js
@@ -2,10 +2,9 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const automationRoutes = require('../routes/automation');
+const automationRoutes = require(path.join(__dirname, 'routes', 'automation'));
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +21,3 @@ app.get('/', function (req, res) {
 
 module.exports = app;
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
