333e6d992af73b3d11b755b4a0be71f23e437967
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-07-23 08:31:33 -0400

    added integration code scraping code

--- server/src/scripts/integration/addButton.js
+++ server/src/scripts/integration/addButton.js
@@ -1,14 +1,32 @@
-const body = document.querySelector("body");
+// const body = document.querySelector("body");
 
-const newButton = document.createElement("button");
+// const newButton = document.createElement("button");
 
-newButton.innerText = "Hybrid Source";
+// newButton.innerText = "Hybrid Source";
 
-body.appendChild(newButton);
+// body.appendChild(newButton);
 
+let itemNumber = document.getElementById("itemNumber").value;
+let poNumber = document.getElementById("poNumber").value;
+let quantity = document.getElementById("quantity").value;
+let vendor = document.getElementById("vendor").value;
 
+const storeNumber = poNumber.split("-")[0];
 
-let itemNumber = document.getElementById("itemNumber").value
-let poNumber = document.getElementById('poNumber').value
-let quantity = document.getElementById('quantity').value
-let vendor = document.getElementById("vendor").value
+const response = await fetch("http://localhost:5000/api/automation/run", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    vendor,
+    storeNumber,
+    itemNumber,
+    poNumber,
+    quantity,
+    pickup : false, // TODO: add pickup option
+    tireRackAccount: "MavisCorp",
+  }),
+});
+const data = await response.json();
+console.log(data);
