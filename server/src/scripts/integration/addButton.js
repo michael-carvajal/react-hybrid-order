14071ff66cff00682f8d619d3c941be5a236bd8d
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-07-22 22:17:56 -0400

    added button to form page and selected order details

---
+++ server/src/scripts/integration/addButton.js
@@ -0,0 +1,14 @@
+const body = document.querySelector("body");
+
+const newButton = document.createElement("button");
+
+newButton.innerText = "Hybrid Source";
+
+body.appendChild(newButton);
+
+
+
+let itemNumber = document.getElementById("itemNumber").value
+let poNumber = document.getElementById('poNumber').value
+let quantity = document.getElementById('quantity').value
+let vendor = document.getElementById("vendor").value
