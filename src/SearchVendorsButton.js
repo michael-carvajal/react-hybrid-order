c2471ea22c09382b2540778be86a2eca6d139cbb
Michael Carvajal <michaelcarvajal.work@gmail.com>
2024-07-29 13:18:30 -0400

    able to go through all vendors

--- src/SearchVendorsButton.js
+++ src/SearchVendorsButton.js
@@ -1,42 +1,53 @@
 import React from "react";
 
 function SearchVendorsButton({ props, setError, setConfirmation }) {
-  const { vendor, itemNumber, poNumber, quantity, pickup, tireRackAccount } =
-    props;
+  const { itemNumber, poNumber, quantity, pickup, tireRackAccount } = props;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setConfirmation([]);
     setError([]);
-    // console.log(vendor, itemNumber, poNumber, quantity, pickup);
+    const vendors = ["NTW", "TIREHUB", "TIRERACK", "ATD", "USA", "MFI"];
     const storeNumber = poNumber.split("-")[0];
+
     try {
-      const response = await fetch("http://localhost:5000/api/automation/run", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          vendor,
-          storeNumber,
-          itemNumber,
-          poNumber,
-          quantity,
-          pickup,
-          tireRackAccount,
-        }),
+      const requests = vendors.map((vendor) => {
+        return fetch("http://localhost:5000/api/automation/run", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            vendor,
+            storeNumber,
+            itemNumber,
+            poNumber,
+            quantity,
+            pickup,
+            tireRackAccount,
+          }),
+        }).then((response) => response.json());
       });
-      const data = await response.json();
-      console.log(data);
-      if (data.error) {
-        setError(data.error);
-      } else if (data.confirmation) {
-        setConfirmation(data.confirmation);
+
+      const results = await Promise.all(requests);
+
+      const errors = results.filter(result => result.error);
+      const confirmations = results.filter(result => result.confirmation);
+
+      if (errors.length > 0) {
+        setError(errors.map(error => error.error));
+      } 
+
+      if (confirmations.length > 0) {
+        setConfirmation(confirmations.map(confirmation => confirmation.confirmation));
       }
+      
     } catch (error) {
       console.log(error);
+      setError(["An error occurred while processing the requests."]);
     }
   };
+
   return (
     <button className="btn btn-danger" onClick={handleSubmit}>
       Search All Vendors
